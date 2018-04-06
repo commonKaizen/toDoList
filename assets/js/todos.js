15ec02c199b9e806fc2b32f128a87b1025245dec
cK <wim.va@mail.com>
2018-04-06 12:48:21 +0200

    delete complete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,9 @@
 $("li").on("click", function () {
     $(this).toggleClass("complete");
 });
+
+$("span").on("click", function () {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+});
